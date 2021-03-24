6b3b2d054b426b7fb2c0675f5c42535a905ede9f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-24 10:05:29 +0100

    v. 0.2

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const debounce = (fn) => {
             cancelAnimationFrame(frame);
         }
         frame = requestAnimationFrame(() => {
-
             fn(...params);
         });
     }
@@ -13,18 +12,32 @@ const debounce = (fn) => {
 
 const onScroll = () => {
     updateNavBarColor();
-    //updateNavPoints();
+    updateNavPoints();
 }
 
 const updateNavBarColor = () => {
-    var head = document.getElementsByClassName("head")[0];
-    head.style.backgroundColor = "rgba(255, 255, 255, " + (window.scrollY / 300) + ")";
-    head.style.color = "rgb(" + (255 - ((window.scrollY / 300) * 255)) + ", " + (255 - ((window.scrollY / 300) * 255)) + ", " + (255 - ((window.scrollY / 300) * 255)) + ")";
+    //var head = document.getElementsByClassName("head")[0];
+    var head = $(".head")[0];
+    $(head).css("backgroundColor", "rgba(255, 255, 255, " + (window.scrollY / 300) + ")");
+    //head.style.backgroundColor = "rgba(255, 255, 255, " + (window.scrollY / 300) + ")";
+
+    head.style.color = "rgb("
+    + (255 - ((window.scrollY / 300) * 255)) + ", " 
+    + (255 - ((window.scrollY / 300) * 255)) + ", " 
+    + (255 - ((window.scrollY / 300) * 255)) + ")";
+
     if (window.scrollY > 200) {
         head.style.boxShadow = "0px 5px 0.5rem rgba(0, 0, 0, 0.5)";
     } else {
         head.style.boxShadow = null;
     }
+}
+
+const updateNavPoints = () => {
+    var list = $(".nav-point").toArray().filter(p => $($(p).attr("href")).position().top + $(".head").height() > window.scrollY);
+    var last = list[list.length - 1];
+    $(".nav-point").not(last).removeClass("selected");
+    $(last).addClass("selected");
 }
 
 const setupSmoothScrolling = () => {
@@ -42,8 +55,30 @@ const setupSmoothScrolling = () => {
     });
 }
 
+var slideIndex = 1;
+
+const advanceSlideShow = (n) => {
+    var i;
+    //var x = document.getElementsByClassName("slider-image");
+    var x = $(".slider-image");
+    if(slideIndex + n > x.length) {
+        slideIndex = 0;
+    }
+    if(slideIndex + n < 0) {
+        slideIndex = x.length - 1;
+    }
+
+    for(i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex].style.display = "block";
+}
+
 $(function() {
     setupSmoothScrolling();
     updateNavBarColor();
+    updateNavPoints();
     document.addEventListener('scroll', debounce(onScroll), { passive: true });
+    document.getElementsByClassName("button-right")[0].addEventListener("click", () => advanceSlideShow(1));
+    document.getElementsByClassName("button-left")[0].addEventListener("click", () => advanceSlideShow(-1));
 });
